fe161709b32a79675e56bb503854e751a2603bc9
“Nahuel <nnahuell.gb@hotmail.com>
2023-05-24 10:18:43 -0300

    nueva funcion dividir

--- dividir.js
+++ dividir.js
@@ -4,9 +4,13 @@
 
 //funcion dividir flecha
 const dividir = (a,b) =>{  a / b; }
+//funcion dividir flecha 2  
+const dividir2 = (a,b) => a / b;
+
 
 module.exports ={
         dividir,
+        dividir2
 }
 
 //Jugando con  crear conficto
