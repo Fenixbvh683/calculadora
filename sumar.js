c1a8d6344882f1eda1dfcd695c4efc8c11b151fc
“Nahuel <nnahuell.gb@hotmail.com>
2023-05-24 10:05:08 -0300

    sumar =>

--- sumar.js
+++ sumar.js
@@ -1,6 +1,9 @@
-const sumar = function (a,b){
-     return a + b;
-    }
+// const sumar = function (a,b){
+//      return a + b;
+//     }
+const sumar = (a,b) =>{
+    return a + b;
+   }
 
 module.exports ={
     sumar,
